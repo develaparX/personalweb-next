020d14b45d21c9288f34c898e56db2674de9c84e
develaparX <srarfian@gmail.com>
2025-02-14 14:53:16 +0700

    testomonial section, making card and headerSection Component

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -18,7 +18,7 @@ export const TapeSection = () => {
   return (
     <div className="py-16 lg:py-24 overflow-x-clip ">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3  -mx-1">
-        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%, black_90%,transparent)]">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex flex-none gap-4 py-3">
             {words.map((w) => (
               <div key={w} className="inline-flex gap-4 items-center">
