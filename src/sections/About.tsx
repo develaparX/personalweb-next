4bd32761e4aa17546ea86192ce3b80acf72d1edf
develaparX <srarfian@gmail.com>
2025-02-25 20:32:14 +0700

    tape animation

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -102,7 +102,7 @@ const hobbies = [
 
 export const AboutSection = () => {
   return (
-    <div className="py-20 lg:py-28">
+    <div id="about" className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,7 +7,10 @@ import HeroOrbit from "@/components/HeroOrbit";
 import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => (
-  <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+  <div
+    id="hero"
+    className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip"
+  >
     <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
       <div
         className="absolute inset-0 -z-30 opacity-5"
